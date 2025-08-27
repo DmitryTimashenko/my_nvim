67b0712b530ae3718a69cac93ec2b879bf419b8b
Dmitry Timashenko <timashenko.dmitry@gmail.com>
2025-08-27 18:04:39 +0300

    cs-fixer from container

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       local uv = vim.loop
       local uid = uv.getuid()
+      local util = require("lspconfig.util")  -- вот здесь!
 
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.php = { "docker_php_cs_fixer" }
@@ -20,60 +21,35 @@ return {
           end
 
           local absolute_path = vim.fn.fnamemodify(filename, ":p")
-          local home_path = vim.fn.expand("~/projects/domain-control-service")
-          local container_path = absolute_path:gsub(
-            vim.pesc(home_path),
-            "/var/www"
-          )
+          local root_dir = util.root_pattern("composer.json", ".git")(absolute_path)
+
+          if not root_dir then
+            vim.notify("Не удалось определить корень проекта")
+            return {}
+          end
+
+          local container_path = absolute_path:gsub(vim.pesc(root_dir), "/var/www")
 
           return {
-            "compose",
-            "--file=.ops/docker-compose.php.yml",
-            "--project-directory=.",
             "exec",
-            "-T",
-            "-u", tostring(uid),
+            "-t",
+            "domain-service_php",
             "php",
             "/var/www/vendor/bin/php-cs-fixer",
             "fix",
             "--using-cache=no",
             "--quiet",
             "--allow-risky=yes",
-            container_path
+            container_path,
           }
         end,
         cwd = function()
-          return "/home/timashenkod/projects/domain-control-service"
+          local bufname = vim.api.nvim_buf_get_name(0)
+          local root_dir = util.root_pattern("composer.json", ".git")(bufname)
+          return root_dir or vim.fn.getcwd()
         end,
         stdin = false,
       }
-
-      -- Автокоманда для принудительной перезагрузки
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "ConformPostFormat",
-        callback = function(args)
-          local bufnr = args.data.bufnr
-          if vim.api.nvim_buf_is_valid(bufnr) then
-            vim.schedule(function()
-              local filename = vim.api.nvim_buf_get_name(bufnr)
-              if filename ~= "" then
-                -- Сохраняем состояние
-                local cursor_pos = vim.api.nvim_win_get_cursor(0)
-                local modified = vim.api.nvim_buf_get_option(bufnr, 'modified')
-                
-                -- Принудительно перезагружаем
-                vim.cmd("silent! edit!")
-                
-                -- Восстанавливаем состояние
-                vim.api.nvim_win_set_cursor(0, cursor_pos)
-                vim.api.nvim_buf_set_option(bufnr, 'modified', modified)
-              end
-            end)
-          end
-        end,
-      })
-
-      return opts
     end,
-  }
+  },
 }
